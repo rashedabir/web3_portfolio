46d96893063bcf1cf0d1780b6765fabbcea7095c
rashedabir <rashedabir.cse@gmail.com>
2024-07-04 15:45:16 +0600

    tiny changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -97,7 +97,7 @@ const Navbar = () => {
                   {nav.title}
                 </Link>
               ) : (
-                <a href={`#${nav.id}`}>{nav.title}</a>
+                <a href={`/#${nav.id}`}>{nav.title}</a>
               )}
             </li>
           ))}
